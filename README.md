2077c671dc182bc5e62960cc4a66077df99668a4
Suhaib Khan <suheb.work@gmail.com>
2016-08-13 01:35:38 +0530

    Adding converter script and README
    Signed-off-by: Suhaib Khan <suheb.work@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+# Beyond Words Tile Sources
+
+### Converter
+
+##### Usage:
+
+```
+python image_converter.py -i <input_file/dir> -o <output_dir>
+```
+
+##### Dependencies:
+- [deepzoom.py](https://github.com/openzoom/deepzoom.py)
---
+++ image_converter.py
@@ -0,0 +1,53 @@
+import sys, getopt, os
+import deepzoom
+
+usage_msg = "Usage: python image_converter.py -i <input_file/dir> -o <output_dir>\n\
+Dependency: deepzoom (https://github.com/openzoom/deepzoom.py)"
+info_msg = "Info: No output directory specified. Default \"output\" directory will be used."
+notfound_msg = "Error: No file or directory found named: "
+
+if __name__ == "__main__":
+	argv = sys.argv[1:]
+	input_path = ''
+	output_path = ''
+	try:
+		opts, args = getopt.getopt(argv,"i:o:")
+	except getopt.GetoptError:
+		print(usage_msg)
+		sys.exit(2)
+
+	for opt, arg in opts:
+		if opt == '-i':
+			input_path = arg
+		elif opt == '-o':
+			output_path = arg
+		else:
+			print(usage_msg)
+
+	if input_path == '':
+		print(usage_msg)
+		sys.exit()
+
+	if output_path == '':
+		print(info_msg)
+		output_path = "output"
+
+	creator = deepzoom.ImageCreator()
+
+	if os.path.isdir(input_path):
+		files = os.listdir(input_path)
+		for file_name in files:
+			input_filename = os.path.join(input_path, file_name)
+			if os.path.isfile(input_filename):
+				output_filename = os.path.join(output_path, file_name + '.dzi')
+				creator.create(input_filename, output_filename)
+				print(input_filename + "--->" + output_filename)
+
+	elif os.path.isfile(input_path):
+		input_filename = input_path
+		output_filename = os.path.join(output_path, input_path + '.dzi')
+		creator.create(input_filename, output_filename)
+		print(input_filename + "--->" + output_filename)
+
+	else:
+		print(notfound_msg + input_path);
